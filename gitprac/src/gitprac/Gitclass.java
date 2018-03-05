bcdc86fcaca9699ef29f5a6f9c0c9d990ca5a334
Akul Bhutani <acool318@gmail.com>
2018-03-05 10:54:16 -0800

    commit with selenium starting

--- gitprac/src/gitprac/Gitclass.java
+++ gitprac/src/gitprac/Gitclass.java
@@ -5,6 +5,9 @@ public class Gitclass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello world");
+WebDriver driver;
+driver.findElement(By.xpath("//input"));
+System.out.println("hello again");
 	}
 
 }
